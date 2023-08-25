b6be965a6b0e3333d0b4ccf80a5cfbe784fbda58
Hovik Yeghikyan <hovikyeghikyan@gmail.com>
2023-08-25 15:23:44 +0300

    homework 1

---
+++ src/classwork/chapter2/Example2.java
@@ -0,0 +1,14 @@
+package classwork.chapter2;
+
+public class Example2 {
+    public static void main(String[] args) {
+        int num = 5;
+        System.out.println("num is " + num);
+        num = 10;
+        System.out.println("now num is " + num);
+        int age = 15;
+        System.out.println("age is " + age);
+        int count = (num + age) * 3;
+        System.out.println("count is " + (age + num + count));
+    }
+}
