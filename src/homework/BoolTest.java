6f0c2e9d950b6e94dcd1c9f67c92d2dae96f90e8
Hovik Yeghikyan <hovikyeghikyan@gmail.com>
2023-08-26 17:08:34 +0300

    homework 1

---
+++ src/homework/BoolTest.java
@@ -0,0 +1,13 @@
+package homework;
+
+public class BoolTest {
+    public static void main(String[] args) {
+        boolean b[] = new boolean[5];
+        b[0] = true;
+        b[3] = true;
+        for (int i = 0; i < 5; i++) {
+            System.out.println(b[i]);
+
+        }
+    }
+}
