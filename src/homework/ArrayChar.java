e3ad04794fc5c32c46f225cbb790a87fac235dcd
Hovik Yeghikyan <hovikyeghikyan@gmail.com>
2023-08-26 13:25:19 +0300

    homework 1

---
+++ src/homework/ArrayChar.java
@@ -0,0 +1,13 @@
+package homework;
+
+public class ArrayChar {
+    public static void main(String[] args) {
+        char[] ch = {'X','Y','Z'};
+        System.out.println(ch[0]);
+        System.out.println(ch[1]);
+        System.out.println(ch[2]);
+        for (int i = 0; i < 3; i++) {
+            System.out.println(ch[i] + 0);
+        }
+    }
+}
