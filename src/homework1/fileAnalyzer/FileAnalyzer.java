0d5a43d0d75adee46fe9d3d7013005c8c754010f
Hovik Yeghikyan <hovikyeghikyan@gmail.com>
2023-11-28 19:43:55 +0300

    changed package homework1.fileAnalyzer

--- src/homework1/fileAnalyzer/FileAnalyzer.java
+++ src/homework1/fileAnalyzer/FileAnalyzer.java
@@ -1,37 +1,80 @@
 package homework1.fileAnalyzer;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 
 public class FileAnalyzer {
 
-    public Map<String, Integer> wordMap(String path) throws IOException {
-        // Читаем файл, составляем мапу слово-количество и возвращаем ее
-        return null;
+    // Читаем файл, составляем мапу слово-количество и возвращаем ее
+    public Map<String, Integer> wordMap(String path) {
+        Map<String, Integer> map = new HashMap<>();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(path))) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                int value = 1;
+                String[] words = line.split(" ");
+                for (String word : words) {
+                    if (map.containsKey(word)) {
+                        map.put(word, map.get(word) + 1);
+                    } else {
+                        map.put(word, value);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
-
-
-    public int totalWordCount(String path) throws IOException {
-        // Читаем файл, подсчитываем общее количество слов
-        return 0;
+        return map;
     }
 
-    public int uniqueWordCount(String path) throws IOException {
-        // Читаем файл, подсчитываем количество уникальных слов
-        return 0;
+    // Читаем файл, подсчитываем общее количество слов
+    public int totalWordCount(String path) {
+        Map<String, Integer> map = wordMap(path);
+        int totalCount = 0;
+        for (Integer value : map.values()) {
+            totalCount = totalCount + value;
+        }
+        return totalCount;
     }
 
-    public Map<String, Integer> topFrequentWords(String path, int n) throws IOException {
-        // Читаем файл, находим топ-N часто встречающихся слов
-        return null;
+    // Читаем файл, подсчитываем количество уникальных слов
+    public int uniqueWordCount(String path) {
+
+        Map<String, Integer> map = wordMap(path);
+        int uniqueCount = 0;
+        for (Integer value : map.values()) {
+            if (value == 1) {
+                uniqueCount++;
+            }
+        }
+        return uniqueCount;
     }
 
-    public int countWordOccurrences(String path, String word) throws IOException {
-        // Читаем файл, находим количество вхождений слова и возвращаем это число
+
+    // Читаем файл, находим топ-N часто встречающихся слов
+    public Map<String, Integer> topFrequentWords(String path, int n) {
+        Map<String, Integer> map = wordMap(path);
+        Map<String, Integer> newMap = new HashMap<>();
+        for (Map.Entry<String, Integer> stringIntegerEntry : map.entrySet()) {
+            if (stringIntegerEntry.getValue() >= n) {
+                newMap.put(stringIntegerEntry.getKey(), stringIntegerEntry.getValue());
+            }
+        }
+        return newMap;
+
+    }
+
+    // Читаем файл, находим количество вхождений слова и возвращаем это число
+    public int countWordOccurrences(String path, String word) {
+        Map<String, Integer> map = wordMap(path);
+        for (Map.Entry<String, Integer> stringIntegerEntry : map.entrySet()) {
+            if (stringIntegerEntry.getKey().equals(word)) {
+                return stringIntegerEntry.getValue();
+            }
+        }
         return 0;
     }
 
--- src/homework1/fileAnalyzer/FileAnalyzerMain.java
+++ src/homework1/fileAnalyzer/FileAnalyzerMain.java
@@ -7,5 +7,9 @@ public class FileAnalyzerMain {
         FileAnalyzer fileAnalyzer = new FileAnalyzer();
         String filePath = "C:\\Users\\Admin\\IdeaProjects\\JavaCore\\src\\homework1\\fileAnalyzer\\file.txt";
         System.out.println(fileAnalyzer.wordMap(filePath));
+        System.out.println("total count of words: " + fileAnalyzer.totalWordCount(filePath));
+        System.out.println("unique count of words: " + fileAnalyzer.uniqueWordCount(filePath));
+        System.out.println("top Frequent Words is: " + fileAnalyzer.topFrequentWords(filePath, 3));
+        System.out.println("count Word Occurrences is: " + fileAnalyzer.countWordOccurrences(filePath, "Hello"));
     }
 }
